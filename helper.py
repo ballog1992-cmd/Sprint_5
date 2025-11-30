f3303f0812390d02035320e8eaae37d311a460b3
Vasyliy Gazizov <ballog666@yandex.ru>
2025-11-30 19:45:43 +0300

    Add project files

--- helper.py
+++ helper.py
@@ -1,23 +1,12 @@
-# from faker import Faker
+from faker import Faker
 
-# faker = Faker()
+faker = Faker()
 
 
-# def generate_registration_data():
-#     name = faker.first_name()
-#     email = faker.email()
-#     password = faker.password(length=6)
+def generate_registration_data():
+    name = faker.first_name()
+    email = faker.email()
+    password = faker.password(length=6)
 
-#     return name, email, password
+    return name, email, password
 
-from selenium import webdriver
-
-def get_chromedriver_version():
-    driver = webdriver.Chrome()
-    try:
-        version = driver.capabilities['chrome']['chromedriverVersion']
-        print(f"ChromeDriver version: {version}")
-    finally:
-        driver.quit()
-
-get_chromedriver_version()
